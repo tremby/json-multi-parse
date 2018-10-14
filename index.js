08b8b7e4474c9d9d2f41954a2628c87651964f29
Bart Nagel <bart@tremby.net>
2018-10-14 02:38:34 -0700

    Add some jsdoc

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const ERROR_REGEX = /^Unexpected token { in JSON at position (\d+)$/;
 
+/**
+ * Parse a string of multiple JSON objects
+ *
+ * @param {string} input String with zero or more JSON objects in series,
+ *                       possibly separated by whitespace
+ * @param {string[]} [acc] Accumulator for internal use
+ * @returns {Object[]} Array of objects
+ */
 function jsonMultiParse(input, acc = []) {
 	try {
 		acc.push(JSON.parse(input));
