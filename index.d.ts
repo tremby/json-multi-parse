251db2e6fea21ec2162e17581e7268f6bc1b211a
Joshua Wu <jwu@aviatrix.com>
2023-05-27 21:37:40 -0700

    Add types, including generic return type

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+/**
+ * Parse a string of multiple JSON objects
+ *
+ * @param input String with zero or more JSON objects in series, possibly
+ *              separated by whitespace
+ * @returns Array of objects
+ */
+export default function jsonMultiParse<T = object>(input: string, options?: { partial?: false }): T[];
+
+/**
+ * Parse a string of multiple JSON objects/values
+ *
+ * @param input String with zero or more JSON objects in series, possibly
+ *              separated by whitespace
+ * @param options Options:
+ * @param options.partial Don't throw an error if the input ends partway
+ *                        through an object. Instead add a property `remainder`
+ *                        to the returned array with the remaining partial
+ *                        JSON string. Default: false
+ * @returns Array of results
+ */
+export default function jsonMultiParse<T = object>(input: string, options: { partial: true }): T[] & { remainder: string };
